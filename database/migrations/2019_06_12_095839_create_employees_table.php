9f561b6da6775669aa80984db28723abc2a89842
Dimas <bleizing97@gmail.com>
2019-06-12 18:09:08 +0700

    Create Migrations and Models (On Progress)

---
+++ database/migrations/2019_06_12_095839_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('nama');
+            $table->tinyInteger('jenis_kelamin')->default(1);   // 1 = Laki-laki, 2 = Perempuan
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('alamat');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
--- database/migrations/2019_06_12_095906_create_vehicles_table.php
+++ database/migrations/2019_06_12_095906_create_vehicles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateVehiclesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('vehicles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->tinyInteger('is_active')->default(1);   // 1 = Aktif, 0 = Tidak Aktif
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->tinyInteger('user_type')->default(1);   // 1 = Karyawan, 2 = Admin, 3 = Petugas
-            $table->rememberToken();
+            $table->bigInteger('user_id')->unsigned();
             $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('vehicles');
     }
 }
