8ee87a7c70f70906610c917a3d3296cf489a5ec8
Dimas <bleizing97@gmail.com>
2019-06-24 07:37:11 +0700

    Fixing Seeder Done

--- database/seeds/ParkirRateSeeder.php
+++ database/seeds/ParkirRateSeeder.php
@@ -13,18 +13,18 @@ class ParkirRateSeeder extends Seeder
      */
     public function run()
     {
+        $parkir_rate_mobil = ParkirRate::create([
+            'satu_jam_pertama' => 5000,
+            'tiap_jam' => 2000,
+            'per_hari' => 50000,
+            'parkir_type' => 1
+        ]);
+
     	$parkir_rate_motor = ParkirRate::create([
     		'satu_jam_pertama' => 2000,
     		'tiap_jam' => 1000,
     		'per_hari' => 20000,
     		'parkir_type' => 2
-    	]);
-
-    	$parkir_rate_mobil = ParkirRate::create([
-    		'satu_jam_pertama' => 5000,
-    		'tiap_jam' => 2000,
-    		'per_hari' => 50000,
-    		'parkir_type' => 1
     	]);
     }
 }
